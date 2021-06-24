cc30efbb24bd04304f73a1670670272d1da2ce52
Tomasz Kukla <79609921+tomasz-kukla@users.noreply.github.com>
2021-06-25 00:26:53 +0200

    Update

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -11,39 +11,46 @@ import { Container, Col, Button } from 'react-bootstrap';
 
 export const HomePage = (props) => {
 
-    var cors = require('cors');
-
     const [orders, setOrders] = useState([]);
+
 
 
     useEffect(() => {
         axios.get("http://127.0.0.1:8083/orders/")
-            .then(res => console.log(res.data),
-            
+            .then(res => {
+                let result = res.data;
+                setOrders(result);
+
+            }
             )
             .catch(console.log)
     }, []);
 
+    if (orders.length)
+        console.log("Orders: " + JSON.stringify(orders[0].orderPositionDTOList, null, 2));
+
+
+
+
+
     const filteredReady = orders.filter(x => x.status === "Przyjęto do realizacji");
+    console.log("Ready: " + JSON.stringify(filteredReady, null, 2));
     const filteredInProgress = orders.filter(x => x.status === "W trakcie realizacji");
+    console.log("Progress: " + JSON.stringify(filteredInProgress, null, 2));
     const filteredDone = orders.filter(x => x.status === "Do odbioru");
 
 
+    const handleForward = ({ id }) => {
+        console.log({ id });
+        const ID = "c95a399b-f019-4e08-b71f-fb8e71a98239";
 
-    // const handleForward = () => {
-    //     console.log(data)
+        axios.put("http://127.0.0.1:8083/order/forward/" + ID + "/")
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(console.log)
 
-    //     axios.post("http://127.0.0.1:8083/, {
-    //         category: data.category,
-    //         origin: data.origin,
-    //         established: data.established,
-    //     })
-    //         .then(res => {
-    //             console.log(res.data)
-    //         })
-    // .catch(console.log)
-
-    // }
+    }
 
     // const handleBackward = () => {
     //     console.log(data)
@@ -62,110 +69,132 @@ export const HomePage = (props) => {
 
 
 
-
-
-
-
     return (
         <Container className="container">
             <div className="row">
                 <div className="col justify-content-center">
                     <H1>Przyjęto do realizacji</H1>
 
-                    <Div className="card text-center border-success mb-3" styles={`width: 20rem`}>
-                        <div className="card-header">
-                            <h5 className="card-title">
-                                Title</h5>
-                        </div>
+                    {filteredReady.map((ready, index) => (
+                        <Div className="card text-center border-danger mb-3" styles={`width: 20rem`}>
+                            <div className="card-header">
+                                <h5 className="card-title" key={ready.id}>
+                                    {ready.status}</h5>
+                            </div>
+                            <div className="card-body" key={ready.id}>
+                                <t className="card-text" >
+                                    <b>Numer stolika: </b>{ready.tableNo}</t>
+                                <br />
+                                <hr />
+                                <h5 className="card-text" key={ready.id}>
+                                    {ready.orderPositionDTOList.map((dishes, index) => (
+                                        <div key={dishes.id}>
+                                            <h5>Do przygotowania: {dishes.dishDTO.name} x {dishes.quantity}</h5>
 
-                        <div className="card-body">
-                            <t className="card-text" >
-                                text</t>
-                            <br />
-                            <t className="card-text" > Text</t>
-                            <br />
-                            <h4 className="card-text">
-                                <b> Text</b></h4>
-                        </div>
+                                        </div>
+                                    ))}
+                                </h5>
+                            </div>
 
-                        <div className="card-footer">
-                            <a
-                                href={'/'}
-                                class="btn btn-success"
-                            >
-                                Zaktualizuj status
-                            </a>
-                        </div>
-                    </Div>
+                            <div className="card-footer">
+
+                                <a
+                                    onClick={handleForward}
+                                    class="btn btn-success"
+                                >
+                                    Zaktualizuj status
+                                </a>
+                            </div>
+                        </Div>
+
+                    ))
+                    }
 
 
                 </div>
                 <div className="col">
                     <H1>W trakcie realizacji</H1>
-                    <Div className="card text-center border-info  mb-3" styles={`width: 20rem`}>
-                        <div className="card-header">
-                            <h5 className="card-title">
-                                Title</h5>
-                        </div>
+                    {filteredInProgress.map((ready, index) => (
+                        <Div className="card text-center border-danger mb-3" styles={`width: 20rem`}>
+                            <div className="card-header">
+                                <h5 className="card-title" key={ready.id}>
+                                    {ready.status}</h5>
+                            </div>
+                            <div className="card-body" key={ready.id}>
+                                <t className="card-text" >
+                                    <b>Numer stolika: </b>{ready.tableNo}</t>
+                                <br />
+                                <hr />
+                                <h5 className="card-text" key={ready.id}>
+                                    {ready.orderPositionDTOList.map((dishes, index) => (
+                                        <div key={dishes.id}>
+                                            <h5>Do przygotowania: {dishes.dishDTO.name} x {dishes.quantity}</h5>
 
-                        <div className="card-body">
-                            <t className="card-text" >
-                                text</t>
-                            <br />
-                            <t className="card-text" > Text</t>
-                            <br />
-                            <h4 className="card-text">
-                                <b> Text</b></h4>
-                        </div>
+                                        </div>
+                                    ))}
+                                </h5>
+                            </div>
 
-                        <div className="card-footer">
-                            <a href={'/'} class="btn btn-danger">
-                                Cofnij status
-                            </a>
-                            <a>     </a>
-                            <a href={'/'} class="btn btn-success">
-                                Zaktualizuj status
-                            </a>
-                        </div>
-                    </Div>
+                            <div className="card-footer">
+                                <a href={'/'} class="btn btn-danger">
+                                    Cofnij status
+                                </a>
+                                <br />
+                                <a href={'/'} class="btn btn-success" onClick={handleForward}>
+                                    Zaktualizuj status
+                                </a>
+                            </div>
+                        </Div>
 
-
+                    ))
+                    }
 
                 </div>
                 <div className="col">
                     <H1>Do odbioru</H1>
-                    <Div className="card text-center border-danger mb-3" styles={`width: 20rem`}>
-                        <div className="card-header">
-                            <h5 className="card-title">
-                                Title</h5>
-                        </div>
+                    {filteredDone.map((ready, index) => (
+                        <Div className="card text-center border-danger mb-3" styles={`width: 20rem`}>
+                            <div className="card-header">
+                                <h5 className="card-title" key={ready.id}>
+                                    {ready.status}</h5>
+                            </div>
 
-                        <div className="card-body">
-                            <t className="card-text" >
-                                text</t>
-                            <br />
-                            <t className="card-text" > Text</t>
-                            <br />
-                            <h4 className="card-text">
-                                <b> Text</b></h4>
-                        </div>
+                            <div className="card-body" key={ready.id}>
+                                <t className="card-text" >
+                                    <b>Numer stolika: </b>{ready.tableNo}</t>
 
-                        <div className="card-footer">
-                            <a href={'/'} class="btn btn-danger">
-                                Cofnij status
-                            </a>
-                            <a>     </a>
-                            <a href={'/'} class="btn btn-success">
-                                Zaktualizuj status
-                            </a>
-                        </div>
-                    </Div>
+                                <br />
+                                <hr />
 
+                                <h5 className="card-text" key={ready.id}>
+                                    {ready.orderPositionDTOList.map((dishes, index) => (
+                                        <div key={dishes.id}>
+                                            <h5>Do przygotowania: {dishes.dishDTO.name} x {dishes.quantity}</h5>
+
+                                        </div>
+                                    ))}
+                                </h5>
+                            </div>
+
+                            <div className="card-footer">
+                                <a href={'/'} class="btn btn-danger">
+                                    Cofnij status
+                                </a>
+                                <br />
+                                <a href={'/'} class="btn btn-success" onClick={handleForward(ready.id)}>
+                                    Zaktualizuj status
+                                </a>
+                            </div>
+                        </Div>
+
+                    ))
+                    }
 
 
                 </div>
-            </div>
-        </Container>
+
+            </div >
+        </Container >
     );
 
 };
